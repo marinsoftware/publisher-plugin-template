d9cc128b84a3c9650506f8a3119d3f734206acf1
Kanwarjeet Khalsa <kkhalsa@C02FQ1PBQ05N.local>
2022-03-14 15:56:29 +0000

    adding sample healthcheck and removing default app code

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,9 @@
+import { HealthModule } from './health/health.module';
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [HealthModule, CatsModule],
   controllers: [],
   providers: [],
 })
