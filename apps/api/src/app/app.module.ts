25c692dac0e5a7c59df1c402a453aebed3793b12
Kanwarjeet Khalsa <kkhalsa@C02FQ1PBQ05N.local>
2022-03-17 13:52:05 +0000

    adding auth guards example

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './auth/auth.module';
 import { HealthModule } from './health/health.module';
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
@@ -8,15 +9,18 @@ import { LoggingInterceptor } from './logging.interceptor';
 
 @Module({
   imports: [
+    AuthModule,
     HealthModule,
     CatsModule,
     ShibeModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/test'),
   ],
   controllers: [],
-  providers: [ {
-    provide: APP_INTERCEPTOR,
-    useClass: LoggingInterceptor,
-  },],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
